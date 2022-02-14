d975f75f230da32c4a9974aa3cba2528d6476db3
ROEL2407 <roel24komen@gmail.com>
2022-02-14 13:56:39 +0100

    Update: fetch data and put out on screen

--- spa/script.js
+++ spa/script.js
@@ -3,16 +3,31 @@ getAndRenderData()
 const artArray = [];
 
 function getAndRenderData() {
-    const getURL = 'https://tribe.api.fdnd.nl/v1/list'
+    const getURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=OoTZzgc6'
     fetch(getURL).then(response => response.json())
         .then(response => {
             console.log(response)
-            response.data.forEach(art => {
-                    artArray.push({
-                        title: art.memberName
-                    });
+            response.artObjects.forEach(art => {
+                artArray.push({
+                    title: art.longTitle,
+                    img: art.headerImage.url,
+                    place: art.productionPlaces
+
+                });
             })
-        }).catch(error => document.body.insertAdjacentHTML('beforebegin', error))
+            showInfo(artArray)
+        }).catch(error => console.log(error))
 }
 
 console.log(artArray);
+
+function showInfo(data) {
+    data.forEach(item => {
+        tempItem = document.createElement('article');
+
+            output = '<img src="' + item.img + '" alt=""><h2>' + item.title + '</h2>';
+
+        tempItem.innerHTML = output;
+        document.getElementById('artitems').appendChild(tempItem);
+    })
+}
