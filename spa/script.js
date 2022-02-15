2f6a2712ac6007533038a75fdab1457d73dba66d
ROEL2407 <roel24komen@gmail.com>
2022-02-15 11:26:47 +0100

    Update: change position on button click

--- spa/script.js
+++ spa/script.js
@@ -10,7 +10,7 @@ function getAndRenderData() {
             response.artObjects.forEach(art => {
                 artArray.push({
                     title: art.longTitle,
-                    img: art.headerImage.url,
+                    img: art.webImage.url,
                     place: art.productionPlaces
 
                 });
@@ -30,4 +30,15 @@ function showInfo(data) {
         tempItem.innerHTML = output;
         document.getElementById('artitems').appendChild(tempItem);
     })
+}
+
+/* --- get all element for use --- */
+search_btn = document.getElementById("search_btn");
+search = document.getElementById("search");
+
+/* If the button "search" is clicked: */
+search_btn.onclick = function() {
+    /* toggle the visibility of the search box */
+    search.classList.toggle("hidden");
+    search_btn.classList.toggle("active");
 }
