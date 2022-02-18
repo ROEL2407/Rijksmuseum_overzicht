1dcbccdba8dda852b20d0fa9605b49d2af6b404f
ROEL2407 <roel24komen@gmail.com>
2022-02-18 09:33:50 +0100

    Update: added search function

--- spa/script.js
+++ spa/script.js
@@ -15,12 +15,13 @@ function getAndRenderData() {
                     id: art.id,
                     title: art.title,
                     img: art.webImage.url,
-                    place: art.productionPlaces
+                    place: art.productionPlaces,
+                    link: art.links.web
 
                 });
             })
-            
-            display.textContent="";
+
+            display.textContent = "";
             display.classList.remove("loading");
             showInfo(artArray);
             click(artArray);
@@ -34,21 +35,43 @@ function showInfo(data) {
         tempItem = document.createElement('a');
         tempItem.setAttribute("class", "art");
         tempItem.id = item.id;
-
-            output = '<article><div><img src="' + item.img.slice(0, -3)+"=s1000" + '" alt=""></div><h2>' + item.title + '</h2></article>';
-
+        output = '<article><div><img src="' + item.img.slice(0, -3) + "=s1000" + '" alt=""></div><h2>' + item.title + '</h2></article>';
         tempItem.innerHTML = output;
         display.appendChild(tempItem);
     })
 }
+
 function click(data) {
-document.getElementsByClassName("art").forEach(element=> {
-    element.addEventListener("click", function(event){
-        event.preventDefault()
-        info_output = '<article="active"><div><img src="' + item.img.slice(0, -3)+"=s1000" + '" alt=""></div><h2>' + item.title + '</h2></article>'
-        this.innerHTML = info_output;
+    Array.from(document.getElementsByClassName("art")).forEach(element => {
+        element.addEventListener("click", function (event) {
+            event.preventDefault();
+            data.forEach(item => {
+                if(element.id === item.id) {
+                    info_output = '<article class="active"><div><img src="' + item.img.slice(0, -3) + "=s1000" + '" alt=""></div><h2>' + item.title + '</h2><p>' + item.place + '</p><a href="' + item.link + '">' + item.link + '</a></article>'
+                    this.innerHTML = info_output;
+                }
+            })
+        });
+    })
+}
+
+function search() {
+    //Empty results
+    display.innerHTML = "";
+  
+    var input = document.getElementById("inputfield");
+  
+    //search on input
+    var search = data.filter(function (d) {
+      return (
+        d.title.toLowerCase().includes(input.value.toLowerCase()) ||
+        d.principalOrFirstMaker.toLowerCase().includes(input.value.toLowerCase())
+      );
     });
-})
+  
+    //TODO: ignore capital letters & empty state
+  
+    showInfo(search);
 }
 
 /* --- get all element for use --- */
@@ -56,7 +79,7 @@ search_btn = document.getElementById("search_btn");
 search = document.getElementById("search");
 
 /* If the button "search" is clicked: */
-search_btn.onclick = function() {
+search_btn.onclick = function () {
     /* toggle the visibility of the search box */
     search.classList.toggle("hidden");
     search_btn.classList.toggle("active");
